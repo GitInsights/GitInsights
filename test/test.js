8e804eefc68077c27ba50035f7d7663d8760ccf9
Eli Xian <elixian@gmail.com>
2015-03-13 23:59:35 -0500

    karma test suite enabled

---
+++ test/test.js
@@ -0,0 +1,17 @@
+describe('GitApi', function(){
+
+  // var GitApi;
+  // beforeEach(module('GitApi'));
+  // beforeEach(inject(function (_GitApi_){
+  //   GitApi = _GitApi_;
+  // }));
+
+  it('should work now', function(){
+    expect(true).to.equal(true);
+  });
+
+  it('should work now', function(){
+    expect(false).to.equal(true);
+  });
+
+});
