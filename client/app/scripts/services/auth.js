758714c01f90a4d01885f04133f80b64d1c3da5e
Eli Xian <elixian@gmail.com>
2015-03-20 23:01:34 -0500

    added comments for clarity

--- client/app/scripts/services/auth.js
+++ client/app/scripts/services/auth.js
@@ -6,6 +6,12 @@ angular.module('gitInsight.auth', [])
 
 Auth.$inject = [];
 function Auth () {
+  //Uses Firebase only to authenticate via Github
+  //Authenticated Github users are allowed to make 2000api calls/hr vs 60 for unauthenticated users
+
+  //create an instance to interact with firebase api.
+  //url refers to Firebase database.
+  //for more information about Firebase please refer to http://www.firebase.com
   var ref = new Firebase("https://boiling-torch-2275.firebaseio.com");
   var githubToken;
 
@@ -19,6 +25,8 @@ function Auth () {
   }
 
   function login () {
+    //creates a popup for authentication via github
+    //closes pop after and sets the githubToken for use in GitApi calls
     ref.authWithOAuthPopup("github", function (error, authData) {
       if (error) {
         console.log("Login Failed!", error);
