a94c4401840be4aa1e753535fa9b7cd977675fa8
Eli Xian <elixian@gmail.com>
2015-03-23 18:55:30 -0500

    finish refactor of homejs

--- client/app/scripts/home/home.js
+++ client/app/scripts/home/home.js
@@ -13,73 +13,47 @@
 
   function HomeController($scope, GitApi, Auth, Chart){
     $scope.currentUser = {};
-    $scope.users = [];
     $scope.loaded = false;
-    $scope.loaded3 = false;
-
-    $scope.graphData = [];
-    $scope.timeStamps = [];
-
-    $scope.getAllWeeklyData = function(username){
-      GitApi.getAllWeeklyData(username)
-        .then(function (data){
-          var weeklyData = GitApi.reduceAllWeeklyData(data)
-          Chart.lineGraph(weeklyData, username);
-          $scope.loaded = true;
-          $scope.users.push($scope.currentUser);
-          $scope.currentUser = {};
-          return data;
-        })
-        .then(function (data) {
-          return GitApi.gatherLanguageData(data);
-        })
-        .then(function (data) {
-          var languages = GitApi.getUserLanguages(data);
-          addPieGraph(languages);
-        });
-    };
-
-    var count = 0
-    var addPieGraph = function (languages){
-      // Limits max user comparison = 2
-      count++
-      if(count > 2){
-        $scope.loaded3 = false;
-        count = 1;
-      } else if (count === 2){
-        $scope.loaded3 = true;
-      }
-      //Changes format from {JavaScript: 676977.4910200321, CSS: 3554.990878681176, HTML: 41.838509316770185, Shell: 4024.4960858041054}
-      // to [{"key": "One", "value": 222}, ... , {"key": "Last", "value": 222}]
-      var languageData = d3.entries(languages)
-
-      // Add second pie chart when comparing users.
-      var chart = "#chart2"
-      if(count === 2){
-        chart = "#chart3"
-      }
-
-      // nvd3 library's pie chart.
-      nv.addGraph(function() {
-        var pieChart = nv.models.pieChart()
-            .x(function(d) { return d.key })
-            .y(function(d) { return d.value })
-            .showLabels(true)
-            .labelType("percent");
-
-          d3.select(chart + " svg")
-              .datum(languageData)
-              .transition().duration(350)
-              .call(pieChart);
-
-        return pieChart;
-      });
-    };
+    $scope.loaded3 = true;
+    $scope.numUsers = 0;
 
     $scope.login = function(){
       Auth.login();
     };
 
+    $scope.getAllWeeklyData = function(username){
+      // first we make a set of queries to get data from all the repo's the user has contributed to.
+      // the process also tags some metadata to help with chaining
+      GitApi.getAllWeeklyData(username)
+        .then(function (data){ 
+          // here we can immediately process the data to draw a line graph of the user's activity
+          var weeklyData = GitApi.reduceAllWeeklyData(data)
+          Chart.lineGraph(weeklyData, username);
+          $scope.loaded = true;
+          $scope.currentUser = {};
+          return data;
+        })
+        .then(function (data) {
+          return GitApi.gatherLanguageData(data);
+          // this returns an array of tuples with the form 
+          // [user contirbutions to this repo, repo language stats, total repo activity] when it resolves
+        })
+        .then(function (data) {
+          // this time the data is processed to create a pie chart that estimates 
+          // the % of the each language the user codes in by taking the repo language stats * (user activity / total repo activity)
+          var languages = GitApi.getUserLanguages(data);
+          $scope.numUsers++;
+          $scope.loaded3 = !($scope.loaded3);
+
+          var config = {};
+          config.chart = "#chart2"
+          if($scope.numUsers % 2 === 0){
+            config.chart = "#chart3"
+          }
+
+          Chart.pieChart(languages, config);
+        });
+    };
   }
 })();
 
--- client/app/scripts/services/chart.js
+++ client/app/scripts/services/chart.js
@@ -12,6 +12,7 @@ function Chart () {
 
   return {
     lineGraph: lineGraph,
+    pieChart: pieChart
   };
 
   function lineGraph (data, username) {
@@ -74,6 +75,31 @@ function Chart () {
     });
   };
 
-}
+  function pieChart (languages, config) {
+    // Limits max user comparison = 2
+    //Changes format from {JavaScript: 676977.4910200321, CSS: 3554.990878681176, HTML: 41.838509316770185, Shell: 4024.4960858041054}
+    // to [{"key": "One", "value": 222}, ... , {"key": "Last", "value": 222}]
+    var languageData = d3.entries(languages)
 
+    // Add second pie chart when comparing users.
+    var chart = config.chart;
+
+    // nvd3 library's pie chart.
+    nv.addGraph(function() {
+      var pieChart = nv.models.pieChart()
+          .x(function(d) { return d.key })
+          .y(function(d) { return d.value })
+          .showLabels(true)
+          .labelType("percent");
+
+        d3.select(chart + " svg")
+            .datum(languageData)
+            .transition().duration(350)
+            .call(pieChart);
+
+      return pieChart;
+    });
+  };
+
+}
 })();
