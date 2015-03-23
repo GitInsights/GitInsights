41d22309ada60aedc6e4cd1a7893afc84be0250a
Eli Xian <elixian@gmail.com>
2015-03-23 15:36:50 -0500

    change graph to show net additions instead of just additions and deletions

--- client/app/scripts/home/home.js
+++ client/app/scripts/home/home.js
@@ -50,30 +50,31 @@
     //TODO: refactor into service
     var addGraphData = function(data){
 
-      var additions = [];
-      var deletions = [];
+      var secondsPerYear = 525600 * 60;
+      var dateNow = new Date() / 1000; //convert to unix
+      var dateXYearsAgo = dateNow - (secondsPerYear * 1);
+
+      var netAdditions = [];
       var unixTimeStamps = [];
       var newTimeStamps = [];
 
       for(var week in data){
         unixTimeStamps.push(+week);
-        additions.push(data[week].a);
-        deletions.push(data[week].d);
+        netAdditions.push(data[week].a - data[week].d);
       }
-      var series1 = {"key": $scope.currentUser.username + "'s Additions", "values": []};
-      var series2 = {"key": $scope.currentUser.username + "'s Deletions", "values": []};
+      var series1 = {"key": $scope.currentUser.username + "'s Net Additions", "values": []};
 
       for(var i = 0; i < unixTimeStamps.length; i++){
-        series1.values.push([unixTimeStamps[i], additions[i]]);
-        series2.values.push([unixTimeStamps[i], deletions[i]]);
+        if (unixTimeStamps[i] > dateXYearsAgo) {
+          series1.values.push([unixTimeStamps[i], netAdditions[i]]);
+        }
       }
 
-      if($scope.userOneData.length >= 4){
+      if($scope.userOneData.length >= 2){
         $scope.userOneData = [];
       }
 
       $scope.userOneData.push(series1);
-      $scope.userOneData.push(series2);
 
       // nv is a nvd3 library object. (on global scope)
       nv.addGraph(function() {
@@ -93,7 +94,7 @@
 
         // Define y axis
         chart.yAxis
-        .domain(d3.range(additions))
+        .domain(d3.range(netAdditions))
         .tickFormat(d3.format('d'));
 
         // append defined chart to svg element
