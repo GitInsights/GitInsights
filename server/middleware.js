2c492d4ff01c741c2406d7d34b78b6cc346a1a3a
Kate Jefferson <barbedbella@gmail.com>
2015-03-24 17:54:57 -0500

    (fix) added semicolons to server files (fix) updated names and urls in package.json and bower files

--- server/middleware.js
+++ server/middleware.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var bodyparser = require('body-parser');
-var morgan = require('morgan')
+var morgan = require('morgan');
 
 module.exports = function (app) {
 
   app.use(bodyparser.json());
   app.use(morgan('dev'));
-  app.use(express.static(__dirname + '/../client'))
+  app.use(express.static(__dirname + '/../client'));
 
   app.get('/', function (req, res) {
     res.sendFile('index.html');
   });
 
-}
+};
